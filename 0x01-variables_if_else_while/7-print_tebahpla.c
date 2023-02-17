83906235890476de66e93101c6a4e183be405974
EtsubGirma <etsubgirma1290@gmail.com>
2023-02-17 09:35:50 -0800

    task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - number 7
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char n;
+
+	for (n = 'z'; n  >= 'a'; n--)
+	{
+		{
+			putchar(n);
+		}
+	}
+putchar('\n');
+return (0);
+}
