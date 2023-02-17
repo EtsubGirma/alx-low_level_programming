2a3a468dd7b901c7401f543b72176440f6510cd0
EtsubGirma <etsubgirma1290@gmail.com>
2023-02-17 09:38:12 -0800

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - number 8
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+return (0);
+}
