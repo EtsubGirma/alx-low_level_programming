30fe201a99ee2500561d78b3468a784436ea9740
EtsubGirma <etsubgirma1290@gmail.com>
2023-02-17 09:21:03 -0800

    task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - using the C
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+return (0);
+}
