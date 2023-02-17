695e19ee0ca9040b73d9d71c6ccbdde1b3b2ea71
EtsubGirma <etsubgirma1290@gmail.com>
2023-02-17 09:11:20 -0800

    task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the way
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char n;
+char m;
+for (n = 'a'; n <= 'z'; n++)
+putchar(n);
+for (m = 'A'; m <= 'Z'; m++)
+putchar(m);
+putchar('\n');
+return (0);
+}
