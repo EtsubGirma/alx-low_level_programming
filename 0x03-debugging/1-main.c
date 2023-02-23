c367d02642f5c7fe42638662ba62c410cc74e45e
EtsubGirma <etsubgirma1290@gmail.com>
2023-02-23 11:12:40 -0800

    task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
-/*
-* main - causes an infinite loop
-* Return: 0
-*/
-int main(void)
+#include "main.h"
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
 {
-	int i;
-	printf("Infinite loop incoming :(\n");
-	i = 0;
-	/* while (i < 10)*/
-	/* {*/
-	/* putchar(i);*/
-	/* }*/
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+	int largest;
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,17 +9,17 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
-	largest = a;
+		largest = a;
 	}
-	else if (b > c)
+	else if (b >= a && b >= c)
 	{
-	largest = b;
+		largest = b;
 	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
 	return (largest);
 }
