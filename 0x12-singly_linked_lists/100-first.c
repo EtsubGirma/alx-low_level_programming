6a37d13f0a0498a1bfb2c644ca14d0e73a9d3fce
EtsubGirma <etsubgirma1290@gmail.com>
2023-04-02 06:08:49 -0700

    task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * first - function executed before main
+ * Return: no return.
+ */
+
 void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow");
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *current_node;
