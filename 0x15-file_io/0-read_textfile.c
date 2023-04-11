bf204d54e1ba5ba1296d4aec041ec4e11fa55191
EtsubGirma <etsubgirma1290@gmail.com>
2023-04-11 01:18:15 -0700

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters)
+
 {
 	int fd;
 	ssize_t lenr, lenw;
@@ -34,4 +35,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (lenr != lenw)
 		return (0);
 	return (lenw);
+
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,7 @@
 #include "main.h"
+
 int create_file(const char *filename, char *text_content)
+
 {
 	int fd, rstatus, i;
 
