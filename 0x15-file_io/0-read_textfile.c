62ae32c9e363311ef874b429bd14670f02f07b13
EtsubGirma <etsubgirma1290@gmail.com>
2023-04-17 13:34:31 -0700

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,8 +5,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
 	int fd;
 	ssize_t lenr, lenw;
@@ -35,5 +41,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (lenr != lenw)
 		return (0);
 	return (lenw);
-
 }
