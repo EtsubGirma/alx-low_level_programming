b99a080f445754aab7b29b9821882f38a8156889
EtsubGirma <etsubgirma1290@gmail.com>
2023-04-17 13:51:26 -0700

    task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _pow - calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
 	unsigned long int num;
@@ -11,6 +18,12 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 	return (num);
 }
 
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
 	unsigned long int divisor, check;
