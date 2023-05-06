02f02f56a7fe2313f9d5503f469f294b8d7cab6d
EtsubGirma <etsubgirma1290@gmail.com>
2023-05-06 06:59:26 -0700

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,10 @@
 #include "main.h"
-void swap_int(int *a, int *b);
-{
-	int s;
-	s = *a;
-	*a = *b;
-	*b = s;
-/*	void swap_int(int *a, int *b)
+
+void swap_int(int *a, int *b)
 {
 	int temp;
 
 	temp = *a;
 	*a = *b;
 	*b = temp;
-}*/
 }
